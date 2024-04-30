62595e1db149591854d22490a6d9c697162bdff2
Yashwenth S <127986257+Yashwenth27@users.noreply.github.com>
2024-04-30 19:59:17 +0530

    Update Home.py

--- NiviMS/Home.py
+++ NiviMS/Home.py
@@ -15,7 +15,7 @@ from firebase_admin import credentials
 
 # Initialize Firebase Admin SDK with service account credentials
 
-cred = credentials.Certificate("NiviMS/connected-3948a-firebase-adminsdk-zthtg-53ccdd7723.json")
+cred = credentials.Certificate("main/NiviMS/connected-3948a-firebase-adminsdk-zthtg-53ccdd7723.json")
 firebase_admin.initialize_app(cred)
 
 # Add elements to the first column
